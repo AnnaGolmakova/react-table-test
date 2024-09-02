0c8025be1a516a0633f9cf080923d6824bf3f556
Anna Golmakova <ann.golmakova@gmail.com>
2024-09-02 18:40:16 +0100

    CRUD API for working with tables

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,18 +1,69 @@
 import checkResponse from "../utils/checkResponse";
+import { DocumentType } from "../model/DocumentType";
 
-const BASE_URL: string = "https://example.com/api";
+const BASE_URL: string = "https://test.v5.pryaniky.com";
 
-function getResource<T>(
-  endpoint: string,
-  options: Record<string, string | number | boolean>,
-): Promise<T> {
+export const authorize = (email: string, password: string) => {
+  return getResource(`/ru/data/v3/testmethods/docs/login`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+};
+
+export const getTableData = (token: string) => {
+  return getResource(`/ru/data/v3/testmethods/docs/userdocs/get`, {
+    method: "GET",
+    headers: {
+      "x-auth": token,
+    },
+  });
+};
+
+export const createTableRow = (payload: DocumentType, token: string) => {
+  return getResource(`/ru/data/v3/testmethods/docs/userdocs/create`, {
+    method: "POST",
+    headers: {
+      "x-auth": token,
+    },
+    body: JSON.stringify(payload),
+  });
+};
+
+export const updateTableRow = (
+  id: string,
+  payload: DocumentType,
+  token: string,
+) => {
+  return getResource(`/ru/data/v3/testmethods/docs/userdocs/set/${id}`, {
+    method: "POST",
+    headers: {
+      "x-auth": token,
+    },
+    body: JSON.stringify(payload),
+  });
+};
+
+export const deleteTableRow = (id: string, token: string) => {
+  return getResource(`/ru/data/v3/testmethods/docs/userdocs/delete/${id}`, {
+    method: "POST",
+    headers: {
+      "x-auth": token,
+    },
+  });
+};
+
+function getResource<T>(endpoint: string, options?: RequestInit): Promise<T> {
   const params = new URLSearchParams(
     options as Record<string, string>,
   ).toString();
 
-  return fetch(`${BASE_URL}/${endpoint}/?${params}`, {
-    method: "GET",
-  }).then((res) => checkResponse<T>(res));
+  return fetch(`${BASE_URL}/${endpoint}/?${params}`, options).then((res) =>
+    checkResponse<T>(res),
+  );
 }
 
 export default getResource;
