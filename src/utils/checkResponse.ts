52dde0b351efc49f74815a0b1d84eb8d42bf2ec8
Anna Golmakova <ann.golmakova@gmail.com>
2024-09-02 20:12:30 +0100

    Additional error handling when handling requests

--- src/utils/checkResponse.ts
+++ src/utils/checkResponse.ts
@@ -2,10 +2,22 @@ import RequestError from "./requestError";
 
 async function checkResponse<T>(res: Response): Promise<T> {
   const json = await res.json();
-  if (res.ok) {
-    return json;
+
+  // If the response is not OK, throw an error
+  if (!res.ok) {
+    throw new RequestError(
+      json.message || "An error occurred",
+      res.status,
+      json,
+    );
   }
-  return Promise.reject(new RequestError(json.message, res.status, json));
+
+  // Check if the response has an `error_code` field
+  if ("error_code" in json && json["error_code"] !== 0) {
+    throw new RequestError(json["error_message"], json["error_code"], json);
+  }
+
+  return json;
 }
 
 export default checkResponse;
